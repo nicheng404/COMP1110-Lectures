e745a3801af2760f21f4b97adf543d7f5ecc654d
Josh Milthorpe <josh.milthorpe@anu.edu.au>
2021-02-22 16:36:32 +1100

    Monday Week 1: J01 HelloWorld and IOTest, J02 BasicImperative

---
+++ src/comp1110/lectures/J01/HelloWorld.java
@@ -0,0 +1,7 @@
+package comp1110.lectures.J01;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/comp1110/lectures/J02/BasicImperative.java
@@ -0,0 +1,17 @@
+package comp1110.lectures.J02;
+
+public class BasicImperative {
+    public static void main(String[] args) {
+        int i = 0;
+        System.out.println("i: " + i);
+        i = i + 7;
+        System.out.println("i: " + i);
+        i = 256;
+        System.out.println("i: " + i);
+        i = 9;
+        System.out.println("i: " + i);
+        if (i > 10) {
+            System.out.println("i is greater than 10");
+        }
+    }
+}
